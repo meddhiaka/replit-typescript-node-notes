8f521a21e2bc11d88e5175c92fa36a3a73351c56
Med Dhia Kassab <mohameddhiakb@gmail.com>
2024-08-04 16:32:52 +0000

    main ts file for running multiples modules/ functions

---
+++ index.ts
@@ -0,0 +1,15 @@
+import promises from "./docs/promises";
+import crypotgraphy from "./docs/cryptography";
+import jwtMain from "./docs/jwt";
+import BufferTest from "./docs/buffer";
+import dateTest from "./docs/date";
+import FileSystemTest from "./docs/filesystem";
+import ExecShell from "./docs/exec";
+
+// promises()
+// crypotgraphy();
+// jwtMain();
+// BufferTest();
+// dateTest();
+// FileSystemTest();
+// ExecShell()
